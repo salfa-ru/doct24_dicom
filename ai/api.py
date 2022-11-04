b2488afd7c9afe56d435fccb19e053afec4932db
PavelPlesk <ppismenny@gmail.com>
2022-11-04 16:09:41 +0300

    api.py

---
+++ ai/api.py
@@ -0,0 +1,23 @@
+import os, shutil
+from processor import LungsAnalyzer
+
+
+def api_commander(**kwargs):
+    try:
+        try:
+            analyzer = LungsAnalyzer(kwargs['id'], segmentation=True)
+        except:
+            data_folder = f"./data/{kwargs['id']}/"
+            if not os.path.exists(data_folder):
+                os.mkdir(data_folder)
+            os.mkdir(data_folder + 'dicom/')
+            # shutil.copy() копируем файлы
+            analyzer = LungsAnalyzer(kwargs['id'], segmentation=True)
+        if kwargs.get('mode') == 'mask':
+            return analyzer.get_mask(kwargs['model'])
+        elif kwargs.get('mode') == 'gen':
+            path = analyzer.get_generation(**kwargs)
+            #здесь будет метод для отправки сгенерированного файла в базу
+            return path
+    except Exception as err:
+        return {'error': str(err)}
